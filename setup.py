39e86369d5179e3d43f9303b13f08b91dd380cae
Gaurav Singh <readeradda@gmail.com>
2025-02-27 22:54:32 +0530

    update name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="router-reboot",
+    name="jio-reboot",
     version="0.1.0",
     author="Gaurav Singh",
     author_email="hello@gauravsingh.co",
