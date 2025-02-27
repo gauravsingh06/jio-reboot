8123c74157e53703f68e602a4c89ffcd8e1f0acc
Gaurav Singh <readeradda@gmail.com>
2025-02-27 23:50:53 +0530

    updated package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="jio-reboot",
+    name="jioreboot",
     version="0.1.9",
     author="Gaurav Singh",
     author_email="hello@gauravsingh.co",
